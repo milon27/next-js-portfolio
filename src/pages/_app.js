861b8a2a6d41e5b0b5f7d3b671f1b99a7fabd9af
milon27 <mdjahidulislammilon@gmail.com>
2021-01-25 03:57:43 +0600

    added loading indicator

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '../../styles/globals.css'
 
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function _app({ Component, pageProps }) {
     return (
         <>
