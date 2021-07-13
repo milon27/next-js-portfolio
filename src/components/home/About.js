604f8013fd7ce11dfecc43d794afb91f5c0be751
milon27 <mdjahidulislammilon@gmail.com>
2021-07-13 22:45:33 +0600

    fix some css

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -9,7 +9,7 @@ export default function About() {
                     alignSelf: "center"
                 }}>
                     <h1>About Me</h1>
-                    <p>I am doing my internship on Software Engineering in the Department of Computer Science and Engineering at IUBAT. I have more than 2 years of Android and Web Development experience on the marketplaces Fiverr and Upwork (completed 150+ projects). Currently, I am working with Android, React native, React JS, Next JS, Node JS, MySQL, Firebase, WordPress and Woo-commerce.</p>
+                    <p style={{ lineHeight: "25px", textAlign: "justify" }}>I am doing my internship on Software Engineering in the Department of Computer Science and Engineering at IUBAT. I have more than 2 years of Android and Web Development experience on the marketplaces Fiverr and Upwork (completed 150+ projects). Currently, I am working with Android, React native, React JS, Next JS, Node JS, MySQL, Firebase, WordPress and Woo-commerce.</p>
                 </div>
                 <img src="dark-circle.svg" style={{ position: "absolute", right: 0, bottom: "-50%", width: "40%" }} alt="" />
             </div>
--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 export default function Hero() {
     return (
+        <div className="hero-bg">
+            <div style={{ margin: "auto", maxWidth: "1000px", height: "100vh" }} className="flex">
+                <div className="hero-text">
+                    <h5 style={{ fontSize: "20px", marginBottom: "5px" }} className="color-white">This is</h5>
+                    <h1 style={{ marginTop: "5px" }}>Md Jahidul <br></br>islam Milon</h1>
+                    <h4>Build mobile & web app for you</h4>
 
-        <div className="flex hero-bg">
-            <div className="hero-text">
-                <h5 style={{ fontSize: "20px", marginBottom: "5px" }} className="color-white">This is</h5>
-                <h1 style={{ marginTop: "5px" }}>Md Jahidul <br></br>islam Milon</h1>
-                <h4>Build mobile & web app for you</h4>
+                    <img src="/dash.png" alt="" className="img-w100p onlyMobile" />
 
-                <img src="/dash.png" alt="" className="img-w100p onlyMobile" />
+                    <Link href="/contact" >
+                        <a className="btn">Contact Me Now </a>
+                    </Link>
 
-                <Link href="/contact" >
-                    <a className="btn">Contact Me Now </a>
-                </Link>
-
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
