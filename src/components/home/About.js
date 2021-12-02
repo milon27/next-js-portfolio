2d50c1afc5fcf75312df379de4570720bc0f8cf1
milon27 <mdjahidulislammilon@gmail.com>
2021-12-02 23:48:25 +0600

    fix pdf in mobile

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -9,7 +9,7 @@ export default function About() {
                     alignSelf: "center"
                 }}>
                     <h1>About Me</h1>
-                    <p style={{ lineHeight: "25px", textAlign: "justify" }}>I have completed my Bachelor of Science in the Department of Computer Science and Engineering at IUBAT. I have more than 2 years of Mobile and Web Development experience on the marketplaces Fiverr and Upwork (completed 200+ projects). Currently, I am working with Node JS, Android, React native, React JS, Next JS, MySQL, Firebase,MongoDb, Rest API, GraphQl, WordPress and Woo-commerce.</p>
+                    <p style={{ lineHeight: "25px", textAlign: "left" }}>I have completed my Bachelor of Science in the Department of Computer Science and Engineering at IUBAT. I have more than 2 years of Mobile and Web Development experience on the marketplaces Fiverr and Upwork (completed 200+ projects). Currently, I am working with Node JS, Android, React native, React JS, Next JS, MySQL, Firebase,MongoDb, Rest API, GraphQl, WordPress and Woo-commerce.</p>
                 </div>
                 <img src="dark-circle.svg" style={{ position: "absolute", right: 0, bottom: "-50%", width: "40%" }} alt="" />
                 <img src="dark-circle-light.png" style={{ position: "absolute", right: 0, bottom: "-50%", width: "40%" }} alt="" />
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Body from './../components/Body';
@@ -5,6 +6,23 @@ import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Resume() {
+    const [width, setWidth] = useState(() => {
+        if (typeof window !== "undefined") {
+            return window.innerWidth
+        } else {
+            return 0
+        }
+    })
+
+    useEffect(() => {
+        const resizeListener = () => {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', resizeListener)
+        return () => {
+            window.removeEventListener('resize', resizeListener)
+        }
+    }, [])
 
 
     return (
@@ -18,7 +36,16 @@ export default function Resume() {
             <Body>
 
                 <div style={{ width: "100%", marginTop: "50px", marginBottom: "50px" }} className="color-white">
+
                     <Document
+                        file="resume.pdf"
+                        className="onlyMobile"
+                    >
+                        <Page pageNumber={1} width={width - 60} scale={1} fixed={true} />
+                    </Document>
+
+                    <Document
+                        className="onlyDesk"
                         file="resume.pdf"
                     >
                         <Page pageNumber={1} scale={1} />
